67fdf6ee85c80acc6144ae2fa2edc19f7bf0aa1e
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-18 15:50:55 +0000

    add newline

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 def uppercase(str):
     upper = {}
-    for i in range(0, (len(str))):
+    for i in range(0, len(str)):
+        j = i + 1
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             num = ord(str[i])
             num -= 32
             upper[i] = chr(num)
-        elif i < (len(str) + 1):
+        else:
             upper[i] = str[i]
-        if i < len(str):
+        if j == len(str):
+            print("{}".format(upper[i]), end="\n")
+        else:
             print("{}".format(upper[i]), end="")
+    
