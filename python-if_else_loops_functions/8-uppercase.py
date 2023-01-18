50440c94f569ecfbba53ceccd11de9259021a933
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-18 17:58:09 +0000

    fix upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper = {}
-    for i in range(0, len(str)):
-        j = i + 1
+    size = len(str)
+    upper = ""
+    for i in range(0, size):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             num = ord(str[i])
             num -= 32
-            upper[i] = chr(num)
+            upper += chr(num)
         else:
-            upper[i] = str[i]
-        if j == len(str) or str == "":
-            print("{}".format(upper[i]))
-        else:
-            print("{}".format(upper[i]), end="")
+            upper += str[i]
+    print("{:s}".format(upper))
