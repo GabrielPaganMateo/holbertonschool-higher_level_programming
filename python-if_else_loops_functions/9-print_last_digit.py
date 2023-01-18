a17d9b110d2459017d7ce421980db10daca8a959
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-18 18:34:13 +0000

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,8 @@
 def print_last_digit(number):
     if number > 0:
         last_digit = number % 10
-        print(last_digit)
+        print(last_digit, end="")
     elif number < 0:
         number *= -1
         last_digit = number % 10
-        print(last_digit)
+        print(last_digit, end="")
