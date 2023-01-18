e9d1578b494dd9981834d953e22bcf18ccee2a61
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-18 22:29:52 +0000

    power

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return (a ^ b)
+    x = a
+    if b == 0:
+        return (1)
+    elif b > 0:
+        for i in range(1, b):
+            x *= a
+    elif b < 0:
+        b *= -1
+        for i in range(1, b):
+            x *= a
+        y = 1/x
+        return (y)
+    return (x)
