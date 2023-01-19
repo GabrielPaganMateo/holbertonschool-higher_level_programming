63d199e16f8018c4913c9d6f32a7a4791a5f0e44
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-19 17:20:51 +0000

    2

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+plus = "+"
+minus = "-"
+multi = "*"
+divid = "/"
+print("{} {} {} = {}".format(a, plus, b, add(a, b)))
+print("{} {} {} = {}".format(a, minus, b, sub(a, b)))
+print("{} {} {} = {}".format(a, multi, b, mul(a, b)))
+print("{} {} {} = {}".format(a, divid, b, div(a, b)))
