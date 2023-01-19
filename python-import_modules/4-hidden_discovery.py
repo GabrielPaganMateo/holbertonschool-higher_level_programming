61588aa58b02651f77ccadd7d748755cb04e4943
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-19 19:39:19 +0000

    ?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     import hidden_4
 
-for i in range(0, len(dir(hidden_4))):
-    str = hidden_4[i]
-    if str[0:2] != "__" and str[-2:] != "__":
-        print(str)
+string = ""
+ls = dir(hidden_4)
+
+for i in range(0, len(ls))
+    string = ls[i]
+    if string[0:2] != "__" and string[-2:] != "__":
+        print(string)
