e2ce631c75e96fede0558440108b4c638738f708
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-23 14:25:50 +0000

    Okay

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,19 +2,7 @@
 
 
 def no_c(my_string):
-    new_string = my_string[:]
-    print(my_string)
-    print(new_string)
 
-    for idx in range(0, len(my_string) - 1):
-        print(idx)
-        if my_string[idx] == "c":
-            new_string = my_string[:idx] + my_string[idx + 1:]
-        elif my_string[idx] == "C":
-            if idx == 0:
-                new_string = my_string[idx + 1:]
-            else:
-                new_string = my_string[:idx] + my_string[idx + 1:]
-
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
 
     return new_string
