19d171f05f9ad961e25521feabbf1c812bf855e9
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-23 14:51:06 +0000

    okay

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for block in matrix:
-        print(block)
+    for idx in range(0, (len(matrix) - 1)):
+        for num in range(0, (len(matrix[idx]) - 1)):
+            print(matrix[idx][num])
