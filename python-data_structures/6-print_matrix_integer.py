94d46c6a0f11ac1e3abe7e908656eb56fe0161ea
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-23 15:55:01 +0000

    Edge case

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
-        print("{}".format('\n'))
+        print("{}".format(""))
     for idx in range(0, len(matrix)):
         for num in range(0, len(matrix[idx])):
             if num == len(matrix[idx]) - 1:
