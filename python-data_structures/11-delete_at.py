d4b2f59bb79d52180e3b7d1f9da9de212617f885
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-23 19:07:29 +0000

    11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+
+    if idx == 0:
+        new_list = my_list[1:]
+    elif idx == len(my_list) - 1:
+        new_list = my_list[:-1]
+    else:
+        new_list = my_list[:idx] + my_list[idx + 1:]
+
+    return new_list
