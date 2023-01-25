50a819425359f1bde393cde46760bea70816a1ac
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-25 13:56:49 +0000

    7-update

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def update_dictionary(a_dictionary, key, value)
+def update_dictionary(a_dictionary, key, value):
     updated = a_dictionary.copy()
     updated[key] = value
     return updated
