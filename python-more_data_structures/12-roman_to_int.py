d773198199723d514567ae24a76eda7e05daec54
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-25 21:44:37 +0000

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50,
-                        "C": 100, "D": 500, "M": 1000}
+                     "C": 100, "D": 500, "M": 1000}
     _int = 0
     num_list = []
     num_list.append(0)
