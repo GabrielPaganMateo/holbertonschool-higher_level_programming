dfc487d57e40e8f38c54eee372c79aba8fb99f20
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-25 21:41:20 +0000

    Very hard

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,10 +7,27 @@ def roman_to_int(roman_string):
     elif roman_string is None:
         return 0
 
-    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_numeral = {"I": 1, "V": 5, "X": 10, "L": 50,
+                        "C": 100, "D": 500, "M": 1000}
     _int = 0
-
+    num_list = []
+    num_list.append(0)
     for char in roman_string:
-        _int += roman_numeral[char]
+        num_list.append(roman_numeral[char])
+
+    num_list.append(0)
+
+    prev_number = 0
+    number = 0
+
+    for num in reversed(num_list):
+        number = num
+
+        if prev_number != 0 and number < prev_number:
+            _int -= number
+            continue
+
+        _int += number
+        prev_number = num
 
     return _int
