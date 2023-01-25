91c77a26d75159214d95178011354c765070b197
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-25 12:33:49 +0000

    Only differences

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,8 @@
 
 
 def only_diff_elements(set_1, set_2):
-    diff_elements = set_1 | set_2
+    diff1 = set_1 - set_2
+    diff2 = set_2 - set_1
+    diff_elements = list(diff1) + list(diff2)
+    diff_elements = set(diff_elements)
     return diff_elements
