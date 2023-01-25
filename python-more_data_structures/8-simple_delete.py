230d800abf36bee384c2a89cb9f694c5d63c823a
gabriel.pagan <gabriel.pagan@holbertonstudents.com>
2023-01-25 14:18:29 +0000

    fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,6 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key, "DANGER") not "DANGER":
+    if a_dictionary.get(key, "DANGER") != "DANGER":
         a_dictionary.pop(key)
     return a_dictionary
