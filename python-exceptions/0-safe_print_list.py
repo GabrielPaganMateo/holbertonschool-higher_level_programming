3a72465a8b94d193d395b2ae75ab243ba57d4705
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-27 07:45:46 -0400

    0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+        return x
+    except IndexError:
+        count = 0
+        for i in my_list:
+            if count == x:
+                break
+            else:
+                count += 1
+        print()
+        return count
