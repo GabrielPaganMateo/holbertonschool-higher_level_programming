6624242695448f1581435028d147f2d9c7c03215
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-27 07:20:52 -0400

    Fix function

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,12 +4,11 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError as Err:
-        pass
+    except ZeroDivisionError:
+        result = None
     finally:
         if b != 0:
             print("Inside Result: {}".format(result))
         else:
-            result = Err
             print("Inside Result: {}".format(result))
         return result
