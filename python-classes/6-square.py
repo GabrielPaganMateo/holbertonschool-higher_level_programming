34463407680ba3cf7b646cc7ab9bb68930adb80d
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-30 14:03:20 -0400

    fix len pos

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -22,7 +22,7 @@ class Square:
         else:
             self._Square__size = size
 
-        if type(position) is not tuple or type(position[0]) is not int or type(position[1]) is not int or len(position) < 2 or position[0] < 0 or position[1] < 0:
+        if type(position) is not tuple or len(position) < 2 or type(position[0]) is not int or type(position[1]) is not int or position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self._Square__position = position
@@ -62,7 +62,7 @@ class Square:
     
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or type(value[0]) is not int or type(value[1]) is not int or len(value) < 2 or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or len(value) < 2 or type(value[0]) is not int or type(value[1]) is not int or value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self._Square__position = value
