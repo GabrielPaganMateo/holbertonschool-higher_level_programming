eaf568adfda3c1bc202e6f80d539a40c8e937168
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-30 09:12:36 -0400

    Square exceptions

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module provides a class that 
+defines a square"""
+
+
+class Square:
+    """Defines a square
+    Attribute:
+        size (int): size of the square, private"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self._Square__size = size
