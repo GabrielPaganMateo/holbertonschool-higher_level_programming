84f36b06eb445031c71343e01c94abda97941e12
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-30 14:36:55 -0400

    line break

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,8 +21,8 @@ class Square:
         else:
             self._Square__size = size
 
-        if (type(position) is not tuple or len(position) < 2
-            or type(position[0]) is not int or type(position[1]) is not int or
+        if (type(position) is not tuple or len(position) < 2 or
+            type(position[0]) is not int or type(position[1]) is not int or
                 position[0] < 0 or position[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
