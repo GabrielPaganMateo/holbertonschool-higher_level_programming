383393551fbd588848ae7a472c56044e5a99e163
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-30 08:15:04 -0400

    Add square init method

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module provides a class
+that defines a square"""
+
+
+class Square:
+    """Definition of square
+    Attributes:
+        _size (int): size of square, private"""
+    def __init__(self, size):
+        self._size = size
