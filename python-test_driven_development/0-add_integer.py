68f6ae74a6ba3ae761635ec38ba86323e07ba388
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-31 15:28:36 -0400

    Traceback

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Module imports the add_integer function, adds two ints"""
 
+
 def add_integer(a, b=98):
-    try:
-        if type(a) is not int:
-            if type(a) is not float:
-                raise TypeError("a must be an integer")
-        if type(b) is not int:
-            if type(b) is not float: 
-                raise TypeError("b must be an integer")
-    except Exception as e:
-        return e
-    else:
+    if type(a) is float:
         a = int(a)
+    if type(b) is float:
         b = int(b)
-        return a + b
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return a + b
