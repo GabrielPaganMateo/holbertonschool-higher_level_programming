8c0443868d6540d29e2734abfe4896273dab146d
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-31 13:37:59 -0400

    except Exception

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,12 +2,17 @@
 
 
 def add_integer(a, b=98):
-    if type(a) is not int:
-        if type(a) is not float:
-            raise TypeError("a must be an integer")
-    if type(b) is not int:
-        if type(b) is not float:
-            raise TypeError("b must be an integer")
+    try:
+        if type(a) is not int:
+            if type(a) is not float:
+                raise TypeError("a must be an integer")
+        if type(b) is not int:
+            if type(b) is not float:
+                raise TypeError("b must be an integer")
+    except Exception as e:
+        print(e)
+        return
+
     a = int(a)
     b = int(b)
 
