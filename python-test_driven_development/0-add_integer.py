405e2da1e3f6256be576cd3580cceeca7a73ac81
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-01-31 14:48:52 -0400

    Return exception message

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module imports the add_integer function, adds two ints"""
 
 def add_integer(a, b=98):
     try:
@@ -7,12 +7,11 @@ def add_integer(a, b=98):
             if type(a) is not float:
                 raise TypeError("a must be an integer")
         if type(b) is not int:
-            if type(b) is not float:
+            if type(b) is not float: 
                 raise TypeError("b must be an integer")
     except Exception as e:
-        print(e)
+        return e
     else:
         a = int(a)
         b = int(b)
-
         return a + b
