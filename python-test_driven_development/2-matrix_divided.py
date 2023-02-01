8bb96e7238b377ee51b24078bd983c8815b8a5e7
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-01 15:16:20 -0400

    fixed?

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,7 @@
 that divides all elements of a matrix
 by any number (int or float)"""
 
+
 def matrix_divided(matrix, div):
     """Function that divides every number
     in every list of a matrix by a specified
@@ -12,15 +13,15 @@ def matrix_divided(matrix, div):
         div (int or float): number to divide by"""
     for a_list in matrix:
         if type(a_list) is not list:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError("""matrix must be a matrix (list of lists) of integers/floats""")
         for number in a_list:
             if type(number) is not int:
                 if type(number) is not float:
-                    raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                    raise TypeError("""matrix must be a matrix (list of lists) of integers/floats""")
     if len(matrix) > 1:
         for i in range(len(matrix)):
             if len(matrix[0]) != len(matrix[1]):
-                raise TypeError('Each row of the matrix must have the same size')
+                raise TypeError("Each row of the matrix must have the same size")
     if type(div) is not int:
         if type(div) is not float:
             raise TypeError('div must be a number')
@@ -34,7 +35,3 @@ def matrix_divided(matrix, div):
         return new_list
 
     return list(map(list_divided, matrix))
-
-
-    
-
