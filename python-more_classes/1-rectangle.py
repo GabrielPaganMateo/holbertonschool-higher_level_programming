a98274668ad9b8f04d36c741417b614c6b7f4ac5
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-03 08:03:14 -0400

    width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of
+a Rectangle class
+"""
+
+
+class Rectangle:
+    """Defines a rectangle
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width 
+        self.height = height
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if value is not type(int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value is not type(int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+    
