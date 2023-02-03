25a97304ff530187e1d85fc1147c2722ce9008f4
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-03 14:02:18 -0400

    area methods removed

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -85,13 +85,15 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
         if isinstance(rect_1, Rectangle) == False:
             raise TypeError('rect_1 must be an instance of Rectangle')
         elif isinstance(rect_2, Rectangle) == False:
             raise TypeError('rect_2 must be an instance of Rectangele')
-        elif rect_1.area > rect_2.area:
+        elif area_1 > area_2:
             return rect_1
-        elif rect_1.area < rect_2.area:
+        elif area_1 < area_2:
             return rect_2
         else:
             return rect_1
