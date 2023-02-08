3f4e1a689fbb2ed2dbcb5564a47b82e9674e7115
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-08 07:15:50 -0400

    Add Task-0 file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+"""
+This module imports a function that returns
+the list of available attributes and methods of
+an object
+"""
+
+
+def lookup(obj):
+    """
+    Returns list of available attributes and
+    methods of an object
+        Args:
+            obj (unknown type): object to verify
+    """
+    return dir(obj)
