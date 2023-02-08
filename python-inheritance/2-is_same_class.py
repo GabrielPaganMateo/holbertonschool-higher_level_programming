f159eb2e99f64729fe31f24b0f733fcf910206fc
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-08 09:47:00 -0400

    Remove undefined variable

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,7 +12,7 @@ def is_same_class(obj, a_class):
     if obj is bool:
         obj = bool
     if a_class is not object:
-        if isinstance(a, a_class):
+        if isinstance(obj, a_class):
             return True
         else:
             return False
