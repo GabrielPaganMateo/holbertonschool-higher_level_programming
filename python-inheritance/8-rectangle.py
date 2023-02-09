e4921e66152c5d25d99538d8ae060c63d93859bc
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-09 06:37:04 -0400

    Add Task-8 Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+"""
+Imports the class Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    A class that defines the methods and
+    fields of a Rectangle 
+    """
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(width)
+        BaseGeometry.integer_validator(height)
+        self._Rectangle__width = width
+        self._Rectangle__height = height
