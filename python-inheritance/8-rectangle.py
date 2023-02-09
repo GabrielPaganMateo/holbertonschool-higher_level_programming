ca9b7e282d825e2d5bd84fe7edbedc2501b95687
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-09 07:00:27 -0400

    Fix style

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,10 +4,11 @@ Imports the class Rectangle
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     A class that defines the methods and
-    fields of a Rectangle 
+    fields of a Rectangle
     """
     def __init__(self, width, height):
         self.__width = width
