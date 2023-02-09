4d574f9ad49165fca414145aea8ea85049e92079
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-09 08:11:36 -0400

    Add square Task-10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Imports the class Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        super().integer_validator("size", size)
+    
+    
+
