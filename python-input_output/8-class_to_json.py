d145dc85d9f8b327f58913f3a7dee25b75a30ae5
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-10 18:41:31 -0400

    Fix function Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,11 +2,10 @@
 """
 class_to_json function
 """
-import json
 
 def class_to_json(obj):
     """
     Returns the dictionary description with simple
     data structure for JSON serialization of an object
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
