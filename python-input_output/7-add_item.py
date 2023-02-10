9052f779abeecfbb897cd142763a6a0d3dd18187
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-10 08:34:15 -0400

    Change permissions and extend

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,6 +11,6 @@ args = sys.argv
 filename = "add_item.json"
 with open(filename, 'a+') as file:
     my_list = []
-    my_list.append(args[1:])
+    my_list.extend(args[1:])
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
