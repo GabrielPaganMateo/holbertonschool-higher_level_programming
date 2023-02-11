ab2be87b4ce3fb8350fee00325d8c1cf7af9e936
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-10 20:10:01 -0400

    Fix style Task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,7 +26,7 @@ class Student:
             return my_dict
         else:
             return self.__dict__
-    
+
     def reload_from_json(self, json):
         for key, value in json.items():
             if key == "first_name":
