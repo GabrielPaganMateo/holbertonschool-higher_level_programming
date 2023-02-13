fe77bf69b7878726ee9287815aefb95a79a5bf3e
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-13 07:17:42 -0400

    Create dirs and add class Task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module with Class 'Base'
+"""
+
+
+class Base:
+    """
+    'base' of all the other classes in this project,
+    the goal is to manage id attribute in all future
+    classes and avoid duplicating the same code
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
