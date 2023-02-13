32fd17f98f5b1baeaf74b45adfe924d1c267558e
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-13 09:50:52 -0400

    Add method tasj 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,9 @@ class Rectangle(Base):
         """
         for hash in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """
+        Returns string representation of rectangle
+        """
+        return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}'
