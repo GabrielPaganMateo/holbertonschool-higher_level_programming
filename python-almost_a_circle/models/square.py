f4a7f040ef2520da51bce16b604f24a629159288
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-13 15:39:23 -0400

    Add square task 9

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that defines a square, inherits from
+    Rectangle which inherits from Base
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return (f'[Square ({self.id}) {self.x}/{self.y} - '
+                f'{self.width}')
+
