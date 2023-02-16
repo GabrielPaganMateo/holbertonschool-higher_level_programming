ea35fcf7b9b1e5dbaf684a578d954b8882d5a04c
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-02-16 17:55:55 -0400

    try one more time

--- python-almost_a_circle/tests/test_everything.py
+++ python-almost_a_circle/tests/test_everything.py
@@ -153,4 +153,4 @@ class TestEverything(unittest.TestCase):
     def test_save_to_file(self):
         Rectangle.save_to_file(None)
         with open("Rectangle.json") as file:
-            self.assertMultiLineEqual('[]', file.read())
+            self.assertEqual('[]', file.read())
