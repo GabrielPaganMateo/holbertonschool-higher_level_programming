67bdd80f6cb9ca7b907b4f990c8f1017c81c6954
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-08 15:55:59 -0400

    t

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- Create table in current database
-CREATE TABLE IF NOT EXISTS first_table (
-    'id' int(10),
-    'name' VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table ('id' int(10), 'name' VARCHAR(256));
