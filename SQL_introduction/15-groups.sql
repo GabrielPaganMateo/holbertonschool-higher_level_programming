c5812a4f9ccdfb00b6e3da54d09408c6945fc4ed
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-08 18:47:00 -0400

    Add number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count same value
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY DESC;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
