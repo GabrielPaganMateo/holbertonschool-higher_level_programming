e0f536f8969eed61e088d757000ea3351bfb2dbc
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 08:28:17 -0400

    Not exits

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create database and table
-CREATE DATABASE hbtn_0d_usa;
-CREATE TABLE states (
+CREATE DATABASE if not exists hbtn_0d_usa;
+CREATE TABLE if not exists states (
     id int not null unique primary key,
     name VARCHAR(256) not null
 );
