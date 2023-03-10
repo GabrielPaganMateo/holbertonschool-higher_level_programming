4712a5d72f06286caa643c76ad76cedd3ec10629
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 10:51:50 -0400

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- all cities
-SELECT cities.id, cities.name FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 JOIN states
-ON cities.state_id = states.name;
+ON cities.state_id = states.id;
