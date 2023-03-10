993b69451fa56e0346558f5d2f429154a1eb7c1f
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 08:35:11 -0400

    6 case

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Create database and table
-CREATE DATABASE if not exists hbtn_0d_usa;
-CREATE TABLE if not exists states (
+CASE
+    when hbtn_0d_usa not exists then CREATE DATABASE if not exists hbtn_0d_usa;
+    when hbtn_0d_use.states not exists then CREATE TABLE if not exists states (
     id int not null unique primary key,
     name VARCHAR(256) not null
-);
+    );
