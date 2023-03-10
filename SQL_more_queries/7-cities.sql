bdf1abd34312f217fe4e2f61395b47f843e7cafa
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 09:16:40 -0400

    cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE if not exists hbtn_0d_usa;
 CREATE TABLE if not exists hbtn_0d_usa.cities (
     id int serial DEFAULT value primary key,
-    state_id int not null foreign key references states(id),
+    foreign key (state_id) references states(id),
     name VARCHAR(256) not null
 );
