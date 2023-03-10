b102dc85fb969237b71950454af2fdb6fcb57374
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 08:48:28 -0400

    serial default

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create database and table
 CREATE DATABASE if not exists hbtn_0d_usa;
 CREATE TABLE if not exists hbtn_0d_usa.states (
-    id int not null unique primary key,
+    id int SERIAL DEFAULT VALUE primary key,
     name VARCHAR(256) not null
     );
