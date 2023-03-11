04f38cbc5104d54e20d00124307f27da4c993983
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-10 23:49:41 -0400

    afafa

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- All shows and genre
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-RIGHT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
+RIGHT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_genres.name;
