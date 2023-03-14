a91c2b661a58336d198a6851511874d09aaeb9df
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 11:17:53 -0400

    string

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+List all cities from the database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = (MySQLdb.connect(host='localhost', port=3306, user=username,
+                          passwd=password, db=database))
+    cur = db.cursor()
+    cur.execute(("SELECT * FROM cities JOIN states ON cities.state_id = states.id"))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
