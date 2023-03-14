ebfa6499d66b83a0c1c8a5c4375d30b24c5f4804
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 07:12:17 -0400

    MySQLdb first try

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that lists all states from database"""
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", port=3306, user="gabriel", passwd="password", db="hbtn_0e_0_usa")
+cur = db.cursor()
+cur.execute("SELECT * FROM states")
