36a0885607f33be65fb6f8dc7738ebd497a1ea16
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 13:59:47 -0400

    %s

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,15 +10,15 @@ if __name__ == '__main__':
     username = argv[1]
     password = argv[2]
     database = argv[3]
-    state_name = argv[4]
+    state_name = {argv[4]}
 
     db = (MySQLdb.connect(host='localhost', port=3306, user=username,
                           passwd=password, db=database))
     cur = db.cursor()
     str1 = f'SELECT cities.name FROM cities JOIN states'
-    str2 = f' ON cities.state_id = states.id WHERE states.name = {state_name}'
+    str2 = f' ON cities.state_id = states.id WHERE states.name = %s'
     str3 = str1 + str2
-    cur.execute(str3)
+    cur.execute(str3, state_name)
     states = cur.fetchall()
     for state in states:
         print(state)
