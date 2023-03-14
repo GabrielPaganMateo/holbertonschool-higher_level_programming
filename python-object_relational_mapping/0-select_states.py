32e60f2fa93181dd4f83e6919889cda8e20dbc74
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 07:16:42 -0400

    More documentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,11 @@ Script that lists all states
 from the database hbtn_0e_0_usa
 """
 import MySQLdb
+"""
+I am currently trying to
+document this module so the
+checkers pass
+"""
 
 db = MySQLdb.connect(host="localhost", port=3306, user="gabriel", passwd="password", db="hbtn_0e_0_usa")
 cur = db.cursor()
