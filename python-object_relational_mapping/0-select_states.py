08739be2baaa9fba6b85541db2d4dcc068931267
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 07:13:35 -0400

    Documentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Script that lists all states from database"""
+"""
+Script that lists all states from database
+"""
 import MySQLdb
+"""Imports """
 
 db = MySQLdb.connect(host="localhost", port=3306, user="gabriel", passwd="password", db="hbtn_0e_0_usa")
 cur = db.cursor()
