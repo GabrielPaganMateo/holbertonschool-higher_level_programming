ae79b86acb731872bddef26c88cb6b9f10df4c3b
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 10:39:44 -0400

    injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/user/bin/python3
+"""
+Write a script that takes in arguments
+and displays all values in the states table
+where name matches the argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    db = (MySQLdb.connect(host='localhost', port=3306, user=username,
+                          passwd=password, db=database))
+    cur = db.cursor()
+    cur.execute(("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                 .format(state_name)))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
