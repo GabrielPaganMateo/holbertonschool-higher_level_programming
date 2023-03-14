050e18956ef978a7c53315f458bd042e6e2495b9
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-14 08:16:59 -0400

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all states with a name
+starting with N"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = (MySQLdb.connect(host='localhost', user=username, 
+                          passwd=password, db=database))
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE N%')
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
