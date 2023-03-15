b877b40a42c72603da92db8a2c4371e96e4405a7
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-15 15:00:58 -0400

    yahdiel hack

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
                             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).all()
-    for item in states:
-        print("{}: {}".format(item.id, item.name))
+    for states in session.query(State).order_by(State.id).all():
+
+        print("{}: {}".format(states.id, states.name))
+    session.close()
