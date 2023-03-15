aa0f925fd19fdecf8e82e844bfdc9fd3741efb71
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-15 16:31:49 -0400

    11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Adds the State object Louisiana
+to the databas
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = (create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2],
+                                    sys.argv[3]), pool_pre_ping=True))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    table_len = len(states)
+    new_state = State(id = table_len + 1, name = "Louisiana")
+    print(new_state.id)
+    session.add(new_state)
+    session.close()
