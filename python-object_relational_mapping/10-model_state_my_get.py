c64f6814d4f353c26ac90e5be32be557ea0f1e25
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-03-15 16:01:20 -0400

    10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Prints the State object with tha name passed
+as argument
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = (create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2],
+                                    sys.argv[3]), pool_pre_ping=True))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        if state.name.find(sys.argv[4]) != -1:
+            print("{}".format(state.id))
+            notfound = 0
+        else:
+            notfound = 1
+    if notfound == 1:
+        print("Not found")
+    session.close()
