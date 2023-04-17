7c2eadeba66563e51e550fc7e5465cf53b15547c
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-17 08:11:23 -0400

    Task 2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const argumentNum = process.argv.length;
+if (argumentNum === 2) {
+  console.log('No argument');
+} else if (argumentNum === 3) {
+  console.log('Argument found');
+} else if (argumentNum === 4) {
+  console.log('Arguments found');
+}
