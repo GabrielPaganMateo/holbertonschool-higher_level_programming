9c460da9eb50c5b5067889106b45fd18293a19b9
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-17 13:48:44 -0400

    Add task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('0');
+} else if (args.length === 3) {
+  console.log('0');
+} else {
+  let big = Number.MIN_SAFE_INTEGER;
+  let small = Number.MIN_SAFE_INTEGER;
+  let i = 2;
+  while (i <= args.length) {
+    if (parseInt(args[i]) > big) {
+      small = big;
+      big = parseInt(args[i]);
+    } else if (parseInt(args[i]) > small && parseInt(args[i]) !== big) {
+      small = parseInt(args[i]);
+    }
+    i++;
+  }
+  console.log(small)
+}
