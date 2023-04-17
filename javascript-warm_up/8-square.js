e280165ab28f560fd0ccf5388a8783e7b05a9202
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-17 11:13:26 -0400

    Fix typo

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@ const size = process.argv[2];
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let side = 'x';
+  let side = 'X';
   let i = 1;
   while (i < size) {
-    side += 'x';
+    side += 'X';
     i++;
   }
   let j = 0;
