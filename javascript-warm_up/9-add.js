12acad5186d7054cc384caef54c1471190487a06
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-17 12:49:04 -0400

    Fix style

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const args = process.argv
+const args = process.argv;
 
 if (args[2] === undefined || args[3] === undefined) {
   console.log(NaN);
 } else {
-    const sum = parseInt(args[2]) + parseInt(args[3])
-    console.log(sum)
+  const sum = parseInt(args[2]) + parseInt(args[3]);
+  console.log(sum);
 }
