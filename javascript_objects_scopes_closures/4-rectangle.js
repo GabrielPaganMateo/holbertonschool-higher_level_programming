ed4e87608f8bfa39fdf6a265b1320cf0f7152ce0
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-18 11:22:59 -0400

    fix style

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,40 +1,39 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-        // empty object
-      } else {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      let i = 0;
-      let length = '';
-      while (i < this.width) {
-        length += 'X';
-        i++;
-      }
-      let j = 0;
-      while (j < this.height) {
-        console.log(length);
-        j++;
-      }
-    }
-
-    rotate () {
-        const tmp_h = this.height;
-        const tmp_w = this.width;
-        this.width = tmp_h;
-        this.height = tmp_w;
-    }
-
-    double () {
-      this.width += this.width;
-      this.height += this.height;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      // empty object
+    } else {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  print () {
+    let i = 0;
+    let length = '';
+    while (i < this.width) {
+      length += 'X';
+      i++;
+    }
+    let j = 0;
+    while (j < this.height) {
+      console.log(length);
+      j++;
+    }
+  }
+
+  rotate () {
+    const tmpH = this.height;
+    const tmpW = this.width;
+    this.width = tmpH;
+    this.height = tmpW;
+  }
+
+  double () {
+    this.width += this.width;
+    this.height += this.height;
+  }
+}
+
+module.exports = Rectangle;
