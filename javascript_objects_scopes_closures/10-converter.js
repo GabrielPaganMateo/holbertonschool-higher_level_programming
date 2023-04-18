af70cdad878b2257a21b96ddd5de817d7f3196e2
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-18 17:22:51 -0400

    removed definition

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const conversion = function (num) {
+  return function (num) {
     return num.toString(base);
   };
-  return conversion;
 };
