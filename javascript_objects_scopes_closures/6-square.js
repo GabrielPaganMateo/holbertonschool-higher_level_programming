2dc29bbbd7a50d21e6e246d821dc92977d2a690e
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-18 13:01:36 -0400

    Add task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const PapaSquare = require('./5-square');
+
+class Square extends PapaSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+        if (c === undefined) {
+            c = 'X'
+        }
+        let i = 0;
+        let length = '';
+        while (i < this.width) {
+          length += c;
+          i++;
+        }
+        let j = 0;
+        while (j < this.height) {
+          console.log(length);
+          j++;
+        }
+      }
+}
+
+module.exports = Square;
