543d7821fa92702029309d2940137bb4f4198cea
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-18 14:27:28 -0400

    fix style

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let num = 0;
 exports.logMe = function (item) {
-    let str = String(num) + ':'
-    console.log(str, item)
-    num += 1;
-}
+  const str = String(num) + ':';
+  console.log(str, item);
+  num += 1;
+};
