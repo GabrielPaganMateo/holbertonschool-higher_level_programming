0e4866f195c1f71ced35a684ff0bfd2ea23ce472
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 08:36:31 -0400

    Fix style

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const process = require('process');
 const url = process.argv[2];
 request.get(url).on('response', function (res) {
-    console.log('code:', res.statusCode)
+  console.log('code:', res.statusCode);
 });
