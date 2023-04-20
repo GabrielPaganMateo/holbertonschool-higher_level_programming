47a2a55ef9f8f08454526386f16845f992ae454e
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 08:05:42 -0400

    Add task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const process = require('process');
 const filePath = process.argv[2];
 const text = process.argv[3];
-fs.writeFile(filePath, text, function (err) {
+fs.writeFile(filePath, text, 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
