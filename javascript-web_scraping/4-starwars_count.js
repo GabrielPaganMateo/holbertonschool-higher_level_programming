1de1cfb2513ea8345cc03c63a88bc2dcd609b192
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 10:23:28 -0400

    fix loop

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request(url, (error, response, body) => {
     const obj = JSON.parse(body);
     const films = obj.results;
     let cameoCount = 0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < results.length; i++) {
       if (films[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
         cameoCount += 1;
       }
