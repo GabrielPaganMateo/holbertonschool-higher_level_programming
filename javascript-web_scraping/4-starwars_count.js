a92d5ded26eb8387d81a92c3fc999ea2416946d3
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 10:22:08 -0400

    Add style

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,13 +7,12 @@ request(url, (error, response, body) => {
     console.log(error);
   } else {
     const obj = JSON.parse(body);
-    const films = obj.results
+    const films = obj.results;
     let cameoCount = 0;
-    for (let i = 0; i < 5; i++)
-    {
-        if (films[i]['characters'].includes("https://swapi-api.hbtn.io/api/people/18/")) {
-            cameoCount += 1
-        }
+    for (let i = 0; i < 5; i++) {
+      if (films[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        cameoCount += 1;
+      }
     }
     console.log(cameoCount);
   }
