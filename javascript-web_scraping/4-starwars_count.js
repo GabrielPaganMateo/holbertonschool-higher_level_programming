ab02c4da4d35b2ed9a8800dde01ccbc2af4d3aa6
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 10:21:30 -0400

    Add task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const obj = JSON.parse(body);
+    const films = obj.results
+    let cameoCount = 0;
+    for (let i = 0; i < 5; i++)
+    {
+        if (films[i]['characters'].includes("https://swapi-api.hbtn.io/api/people/18/")) {
+            cameoCount += 1
+        }
+    }
+    console.log(cameoCount);
+  }
+});
