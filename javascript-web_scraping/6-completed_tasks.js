52c17cc5bd0cb3aa8a0e80909657c9ecfce18dea
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-04-20 21:32:39 -0400

    Task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const obj = {};
+    const tasks = JSON.parse(body);
+    let taskCount = 0;
+    let userCount = 1;
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].userId !== userCount) {
+        userCount++;
+        taskCount = 0;
+      }
+      const user = tasks[i].userId;
+      if (tasks[i].completed === true) {
+        taskCount++;
+      }
+      obj[user] = taskCount;
+    }
+    console.log(obj);
+  }
+});
